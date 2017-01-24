be39b4aaa50eec74aa4f1f7d513423ae3915a242
Ben Jay <fakebenjay@gmail.com>
2017-01-24 02:08:35 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,13 @@
+sentence = "And after all, hare did have a spare pair."
+
+def reverse_each_word(sentence)
+  array = sentence.split(" ")
+  yarra = []
+
+  array.collect do |word|
+    dorw = word.reverse
+    yarra << dorw
+  end
+
+  ecnetnes = yarra.join(" ")
+end
